b56212a83feda654b1b60a8540dc4ea52fd8d6ad
Ruslan Rajaobelina <85942176+leruslerus42@users.noreply.github.com>
2022-12-08 05:48:10 +0100

    Update Channel.cpp

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,28 +1,88 @@
 #include "../includes/Channel.hpp"
 
-Channel::Channel() {}
+// to create a channel -> in USER: if (user parse tipes #join) -> Channel:createChannel (with static method); if doesnt work, delete createChannel and in User do new Channel()
+
+
+Channel::Channel() {setName(const std::string name);}
 Channel::~Channel() {}
 
-// void Channel::setName(const std::string& name)
-// {
-// 	//checks if valid:
-// 	// must begin with &#!+
-// 	// lenght <= 50
-// 	// no spaces, no conrol G, no comma, no colon in name
-// 	//duplication of channels: channel names must not be reused
+void        Channel::setName(const std::string name){
+    std::string name_init = "&#!+";
+    int i = 0;
 
-// 	this->name = name;
-// }
+    if (name.empty())
+        return ;
+    while (name_init[i])
+    {
+        if (name_init[i] != name[0])
+            break ;
+        i++;
+        if (i == 4)
+            return ;
+    }
 
-// std::string& Channel::getName()
-// {
-// 	return this->name;
-// }
+    if (name.size() > 50)
+        this->_name = name.resize(50);
+    else
+        this->_name = name;
 
-// void setTopic(const std::string& topic);		//implement
-// std::string& getTopic();						//implement
+    i = 0;
+    while (i < name.size())
+    {
+        if ((this->_name[i] == ' ') || (this->_name[i] == ':') || (this->_name[i] == ','))
+        {
+            std::cout << "Invalid Channel name" << std::endl;
+            return ;
+        }
+    }
 
-// void ChangeTopic(const std::string& newTopic)
-// {
-// 	setTopic(newTopic);
-// }
+    for (std::vector<Channel *>::iterator it = _channels.begin(); it != _channel.end(); ++it)
+    {
+        if (it->_name == this->_name)   
+        { 
+            std::cout << "Channel already exists!" << std::endl;
+            return ;
+        }
+    }
+    _channels.push_back(*this);
+    /*implement function error handling instead of all this. Maybe use some boolean, return true or false.*/
+}
+std::string Channel::getName() const{return this->_name;}
+
+void static Channel::&createChannel()
+{
+        new Channel();
+}
+
+void        Channel::deleteChannel()
+{
+    if (_users.size() == 0)
+        delete this; //suicide
+}
+
+
+void        Channel::setTopic(const std::string topic){this->_topic = topic;}
+std::string Channel::getTopic() const{return this->_topic;}
+
+void Channel::changeTopic(const std::string newTopic) {
+    setTopic(newTopic); 
+    std::cout << "The topic of the channel was updatet!" << std::endl;
+}
+
+
+void		Channel::addUser(User *newUser){
+    _user.push_back(newUser);
+}
+void		Channel::deleteUser(User *user){
+     for (std::vector<User *>::iterator it = _users.begin(); it != _users.end(); ++it)
+        if (*it == user)
+            _users.erase(it);
+    
+    deleteChannel();
+
+}
+
+
+
+
+
