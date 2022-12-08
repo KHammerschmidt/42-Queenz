2d8dc5576c0685e62c5e20e19f6029ea0a63a497
Ruslan Rajaobelina <85942176+leruslerus42@users.noreply.github.com>
2022-12-08 07:08:17 +0100

    Added channel mode

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -2,8 +2,10 @@
 
 // to create a channel -> in USER: if (user parse tipes #join) -> Channel:createChannel (with static method); if doesnt work, delete createChannel and in User do new Channel()
 
+//add flags to user (as var in class user): bool creator = false and bool op = false
 
-Channel::Channel() {setName(const std::string name);}
+
+Channel::Channel() {setName(const std::string name);_maxUsers = 200;}
 Channel::~Channel() {}
 
 //NAME
@@ -50,7 +52,7 @@ void        Channel::setName(const std::string name){
 }
 std::string Channel::getName() const{return this->_name;}
 
-void static Channel::&createChannel()
+void static Channel::createChannel()
 {
         new Channel();
 }
@@ -72,8 +74,16 @@ void Channel::changeTopic(const std::string newTopic) {
 
 
 void		Channel::addUser(User *newUser){
+    if (_users.size() > _maxUsers)
+    {
+        std::cout << "Channel already reached the maximal number of users!"
+        return;
+    }
     _user.push_back(newUser);
+    if (_users.size() == 0)
+        newUser->creator = true;
 }
+
 void		Channel::deleteUser(User *user){
      for (std::vector<User *>::iterator it = _users.begin(); it != _users.end(); ++it)
         if (*it == user)
@@ -83,7 +93,37 @@ void		Channel::deleteUser(User *user){
 
 }
 
+void        Channel::setMode(const std::string mode, const User &user1, const User &user2)
+{
+    if (user1->op == false || user1->creator == false) 
+        return ;
 
+    // O - give "channel creator" status;
+    if (mode == 'O')
+    {
+        if (user1->creator == true)
+            user2->creator ==true;
+    }
+    // Ban (+b)
+    if (mode == 'b')
+        deleteUser(user2);
+    // INVITE  - Invite a client to an invite-only channel (mode +i)
+    if (mode == 'i')
+        addUser(user2);
+    //     o - give/take channel operator privilege;
+    if (mode == 'o')
+    {
+        if (user2->op == true)
+            user2->op == false;
+        else
+            user2->flag == true;
+    }
+    //     l - set/remove the user limit to channel;
+    if (mode == 'l')
+        _maxUsers = 10^1000000;
+
+
+}
 
 
 
